3444e53a98157b9b7411b74ce3e645f4f865caf9
Sparsh Jain <sjshiva8287@gmail.com>
2025-06-30 17:01:02 +0530

    Undo the changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Add the following two lines.
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  
-  // keep any other options you already have
-};
+const nextConfig = {};
 
 export default nextConfig;
